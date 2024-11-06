804ba3c76b31a2b4e51398b9287eb90014c597c1
hunghg255 <giahunghust@gmail.com>
2024-11-06 18:21:40 +0700

    chore: update lint

--- src/components/menus/components/BubbleMenuTwitter.tsx
+++ src/components/menus/components/BubbleMenuTwitter.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-useless-fragment */
-import { useCallback, useMemo, useState } from 'react'
+import { useCallback, useState } from 'react'
 
 import type { Editor } from '@tiptap/react'
 import { BubbleMenu } from '@tiptap/react'
