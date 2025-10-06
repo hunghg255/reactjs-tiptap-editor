2c427645ed47b32c4f375f1f354cccdd6a124520
chris <zhangyunfei@lightdigit.ai>
2025-10-06 10:25:16 +0800

    feat(pdf): add the exportToPdf command

--- src/extensions/ExportPdf/ExportPdf.ts
+++ src/extensions/ExportPdf/ExportPdf.ts
@@ -5,8 +5,17 @@ import { printEditorContent } from '@/utils/pdf';
 
 import type { GeneralOptions, PaperSize, PageMargin } from '@/types';
 
+declare module '@tiptap/core' {
+  interface Commands<ReturnType> {
+    exportPdf: {
+      exportToPdf: () => ReturnType
+    }
+  }
+}
+
 export interface ExportPdfOptions extends GeneralOptions<ExportPdfOptions> {
   paperSize: PaperSize;
+  title?: string;
   margins: {
     top?: PageMargin;
     right?: PageMargin;
@@ -21,6 +30,7 @@ export const ExportPdf = /* @__PURE__ */ Extension.create<ExportPdfOptions>({
     return {
       ...this.parent?.(),
       paperSize: 'Letter',
+      title: 'Echo Editor',
       margins: {
         top: '0.4in',
         right: '0.4in',
@@ -39,6 +49,15 @@ export const ExportPdf = /* @__PURE__ */ Extension.create<ExportPdfOptions>({
           disabled: false,
         },
       }),
+    };
+  },
+  addCommands() {
+    return {
+      exportToPdf:
+        () =>
+          ({ editor }) => {
+            return printEditorContent(editor, this.options);
+          },
     };
   },
 });
